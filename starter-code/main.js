5233dfaed30c713df745ec629cff43c5a29d276d
Malahat Jamei <Malahat Jamei>
2016-12-03 15:04:29 +1100

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "Queen";
+var	cardTwo = "Queen";
+var	cardThree = "King";
+var	cardFour = "King";
+
+ console.log(cardOne == cardTwo);	
+ 	console.log(cardOne == cardFour);
